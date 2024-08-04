002660e10b99cfa3b2d78b1f3314d5e77e360f89
WyvernAllow <bloodflame2535@gmail.com>
2024-08-04 12:58:08 +0200

    use valid watchdog code

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi::helpers::init(&mut system_table).unwrap();
     system_table
         .boot_services()
-        .set_watchdog_timer(0, 0, None)
+        .set_watchdog_timer(0, 80000, None)
         .expect("Failed to set watchdog timer.");
 
     println!("Welcome to Hentai OS!");
